334b6189384e6bfbf5066eb08b359db8a7466d0c
travisp <paul.travis@michigansugar.com>
2025-11-15 20:35:49 -0500

    Committing so I can pull

--- index.py
+++ index.py
@@ -1,66 +1,57 @@
 from flask import Flask
 from markupsafe import escape
+from flask import render_template
 
 app = Flask(__name__)
 
 
+def is_logged_in():
+    return False  # TODO: Define function
+
+def bootstrap_link():
+    return '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">'
+
+
 @app.route("/")
 def index():
-    html = """
-        <!doctype html>
-
-        <html lang="en">
-            <head>
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <title>Spoodazo</title>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-            </head>
-
-            <body>
-                <div class="container">
-                    <div class="card">
-                        <div class="card-header">
-                            Welcome to Spoodazo
-                        </div>
-                        <div class="card-body">
-                            Stuff goes here!
-                        </div>
-                    </div>
-                </div>
-
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-            </body>
-        </html>
-    """
-
-    return html
+    return render_template('./Spoodazo.html')
 
 
 @app.route("/login")
 def login():
-   html = """
-    <!doctype html>
-    
-    <html lang="en">
-        <head>
-            <title>Login</title>
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Tagesschrift&display=swap" rel="stylesheet">
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-        </head>
+    html = """
+        <!doctype html>
+        <html lang="en">
+            <head>
+                <title>Login</title>
+                <link rel="preconnect" href="https://fonts.googleapis.com">
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Tagesschrift&display=swap" rel="stylesheet">
+                {bootstrap_link}
+            </head>
 
-        <body>
-            <div class="container">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>Login</h4>
-                    </div>
-                    <div class="card-body">
+            <body>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-3">
+                            &nbsp;
+                        </div>
+                        <div class="col-3">
+                            <div class="card mt-3">
+                                <div class="card-header">
+                                    <h4>Login</h4>
+                                </div>
+                                <div class="card-body">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            &nbsp;
+                        </div>
                     </div>
                 </div>
-            </div>
-        </body>
-    </html>
-   """
+            </body>
+        </html>
+    """.format(bootstrap_link=bootstrap_link())
+
+    return html
